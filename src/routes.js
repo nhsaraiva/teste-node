6b2fcfe1d6e595d0dcd0b589b2278287f4c177fa
Nathanael <natham_11@hotmail.com>
2020-07-02 20:51:36 -0300

    teste porta padrao

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,11 @@ const routes = new Router();
 
 import UserController from './app/controller/UserController';
 
+routes.get('/', (req, res)=>{
+    return res.json({
+        ok:true
+    });
+});
 routes.get('/users', UserController.store);
 
 export default routes;
